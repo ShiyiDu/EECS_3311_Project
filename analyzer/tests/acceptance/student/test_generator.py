a3af6307f5974085cd9f1d89f713dfe26457ecf4
Leo <shiyi.du@qq.com>
2019-12-05 01:12:47 -0500

    bug fixes

---
+++ analyzer/tests/acceptance/student/test_generator.py
@@ -0,0 +1,16 @@
+import random
+
+def command(choose):
+	constants = 
+
+f = open("test14.txt", "w+")
+for i in range (100):
+	for i in range(random.randrange(1, 17)):
+#		setting up chess
+		f.write(command(0) + '\n')
+	f.write(command(1) + '\n')
+	for i in range(random.randrange(1, 400)):
+		f.write(command(3) + '\n')
+	for i in range(random.randrange(1, 30)):
+		f.write(command(-1) + '\n')
+f.close
